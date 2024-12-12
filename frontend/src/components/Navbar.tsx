43251f3cd5f2c78e00eceec7054c75d9818872ac
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-12 16:08:37 -0500

    Made a basic navbar

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export default function Navbar() {
+  return(
+    <nav className="h-[80px] min-w-svw bg-zinc-800 flex gap-4 py-4 px-8 justify-between items-center text-white">
+      <h1> Logo </h1>
+      <ul className="flex gap-8">
+        <li> Page 1</li>
+        <li> Page 1</li>
+        <li> Page 1</li>
+        <li> Page 1</li>
+      </ul>
+    </nav>
+  )
+}
