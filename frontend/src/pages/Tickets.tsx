6fbbb86e84220abf112fe8b80f257aa7c93be20a
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-12 16:39:27 -0500

    Added input type file and state storing the File. Experimenting with files on the frontend.

--- frontend/src/pages/Tickets.tsx
+++ frontend/src/pages/Tickets.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react"
+
 export default function Tickets() {
+  const [img, setImg] = useState<File | null>();
+  console.log(img);
   return(
     <div className="bg-zinc-700 grow p-8">
       hello
+      <input onChange={(e) => {
+        if (e.target.files != null) {
+          setImg(e.target.files[0]);
+        }
+      }} type="file"/>
     </div>
   )
 }
