0d1776e61e8efab13c871897a408d1d9cb6f406e
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-12 21:07:43 -0500

    Added routes for / /login /signup and associated components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import Login from "./components/Login"
 import Navbar from "./components/Navbar"
 import Tickets from "./pages/Tickets"
+import SignUp from "./components/SignUp"
 
 function App() {
   return (
     <div className="min-h-svh min-w-svw bg-zinc-900 flex flex-col">
       <Navbar />
-      <Tickets />
+      <Routes>
+        <Route path="/" element= { <Tickets /> } />
+        <Route path="/login" element={ <Login /> } />
+        <Route path="/signup" element={ <SignUp /> } />
+      </Routes>
     </div>
   )
 }
