ed3c543fec023c1b646eda2f72e9dc1b446d8e45
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-12 20:26:42 -0500

    Created a basic user controller for testing get all and cookies from request and sending cookie as response. Will use cookies for JWT

---
+++ backend/src/main/java/project1/app/Controllers/UserController.java
@@ -0,0 +1,43 @@
+package project1.app.Controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+import project1.app.Models.User;
+import project1.app.Repository.UserRepository;
+import project1.app.Service.UserService;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+  UserService userService;
+
+  public UserController(UserService userService) {
+    this.userService = userService;
+  }
+
+  @GetMapping("/login")
+  public String LoginHandler(@CookieValue(value = "token", defaultValue = "nothing") String token, HttpServletResponse res) {
+    System.out.println(token);
+    Cookie cookie = new Cookie("token", "test");
+
+    res.addCookie(cookie);
+
+    return "Logged in";
+  }
+
+  @GetMapping("test")
+  public List<User> getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+  
+  
+}
