47b71017f306eae9b01fccfdf2652a04fc017f64
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-12 20:25:51 -0500

    Created user repository

---
+++ backend/src/main/java/project1/app/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package project1.app.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import project1.app.Models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+  
+}
