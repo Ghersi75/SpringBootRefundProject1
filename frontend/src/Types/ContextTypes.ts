bdc11e87daa8b9dab6f003e355ad523d00ca92d3
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-14 18:33:08 -0500

    Updated user info type to include an optional profile pic

--- frontend/src/Types/ContextTypes.ts
+++ frontend/src/Types/ContextTypes.ts
@@ -6,10 +6,15 @@ export interface ChildrenPropType {
 
 export interface UserInfoType {
   username: string,
-  email: string
+  email: string,
+  profilePicLink: string | null
 }
 
 export interface UserContextType {
   userInfo: UserInfoType | null,
   setUserInfo: React.Dispatch<React.SetStateAction<UserInfoType | null>>
+}
+
+export interface UserInfoCookieType {
+  user: UserInfoType
 }
