13c4b9de95ce6f7daf7a26611782f4873d5d6f0d
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-14 18:32:42 -0500

    Added cookies provider to the app in order to access user info cookie

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { UserProvider } from './Context/UserContext.tsx'
+import { CookiesProvider } from 'react-cookie'
 
 createRoot(document.getElementById('root')!).render(
-  <UserProvider>
-    <BrowserRouter>
-      <StrictMode>
-        <App />
-      </StrictMode>
-    </BrowserRouter>
-  </UserProvider>
+  <CookiesProvider>
+    <UserProvider>
+      <BrowserRouter>
+        <StrictMode>
+          <App />
+        </StrictMode>
+      </BrowserRouter>
+    </UserProvider>
+  </CookiesProvider>
 )
