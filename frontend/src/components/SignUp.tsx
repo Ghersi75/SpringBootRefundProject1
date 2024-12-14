2f3e1978b6c680428a433f77570b4478eb295259
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-14 14:33:56 -0500

    Created basic sign up logic with props being passed down from parent component.

--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -1,7 +1,35 @@
-export default function SignUp() {
+import { useState } from "react"
+import { SignInFormPropsType } from "../Types/UserFormTypes";
+
+export default function SignUp({ email, setEmail, username, setUsername, password, setPassword }: SignInFormPropsType) {
+  const [showPass, setShowPass] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, setState: React.Dispatch<React.SetStateAction<string>>) => {
+    setState(e.target.value);
+  }
+
+  const handleShowPassword = () => {
+    setShowPass((prev) => !prev)
+  }
+
+  const handleSignUp = () => {
+    return
+  }
+
   return (
-    <div>
-      SignUp
+    <div className="grow flex justify-center items-center">
+      <form className="bg-zinc-800 p-8 rounded-l flex flex-col gap-4" onSubmit={(e) => {e.preventDefault()}}>
+        <label className="text-white"> Email: </label>
+        <input className="bg-zinc-700 rounded p-2 text-white"  onChange={(e) => { handleChange(e, setEmail) }} value={email} />
+        <label className="text-white"> Username: </label>
+        <input className="bg-zinc-700 rounded p-2 text-white"  onChange={(e) => { handleChange(e, setUsername) }} value={username} />
+        <label className="text-white"> Password: </label>
+        <div className="relative">
+          <input className="bg-zinc-700 rounded p-2 text-white" type={!showPass ? "password" : ""} onChange={(e) => { handleChange(e, setPassword) }} value={password}/>
+          <div className="absolute right-0 top-1/2 -translate-y-1/2 right-4 text-white hover:underline cursor-pointer" onClick={handleShowPassword}> show </div>
+        </div>
+        <button className="bg-zinc-700 p-2 rounded text-white" onClick={handleSignUp}> Sign Up </button>
+      </form>
     </div>
   )
 }
