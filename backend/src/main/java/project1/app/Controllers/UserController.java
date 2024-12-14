5c3dd937cd15e46dbf6f81b22e186338d0a2138d
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-14 16:14:43 -0500

    Added getAllUsers and signUp endpoints for testing

--- backend/src/main/java/project1/app/Controllers/UserController.java
+++ backend/src/main/java/project1/app/Controllers/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import project1.app.DTO.UserSignUpDTO;
 import project1.app.Models.User;
 import project1.app.Service.UserService;
 
@@ -15,8 +17,11 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-
+// TODO: At some point, figure out how to trim strings from request body before they're turned into objects
+// " username" should be treated as "username" for example
 @RestController
 @RequestMapping("/user")
 @CrossOrigin(origins = "http://localhost:5173")
@@ -25,6 +30,11 @@ public class UserController {
 
   public UserController(UserService userService) {
     this.userService = userService;
+  }
+
+  @GetMapping("/all")
+  public List<User> GetAllUsersHandler() {
+    return this.userService.getAllUsers();
   }
 
   @GetMapping("/login")
@@ -36,16 +46,17 @@ public class UserController {
 
     return Map.of("logged_in", false);
   }
-
-  @GetMapping("test")
-  public List<User> getAllUsers() {
-    return this.userService.getAllUsers();
-  }
   
   @GetMapping("/check-username") 
   public Map<String, Boolean> checkUsername(@RequestParam String username) {
     System.out.println("Username: " + username);
 
     return Map.of("isAvailable", this.userService.isUsernameAvailable(username));
+  }
+
+  @PostMapping("/signup")
+  public String SingUpHandler(@Valid @RequestBody() UserSignUpDTO userInfo) {
+    
+    return "Success";
   }
 }
