2bd1315467edda0ea2ffd3456feed8f0f730a59b
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-14 18:34:14 -0500

    Added links to login and sign up pages as well navbar user info component to navbar right side

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,13 +1,32 @@
+import { useContext } from "react"
+import { Link } from "react-router-dom"
+import { UserContext } from "../Context/UserContext"
+import NavbarUserInfo from "./NavbarUserInfo";
+
 export default function Navbar() {
+  const content = useContext(UserContext);
+
+  if (!content) {
+    throw new Error("Error");
+  }
+
+  const { userInfo } = content;
+
   return(
     <nav className="h-[80px] min-w-svw bg-zinc-800 flex gap-4 py-4 px-8 justify-between items-center text-white">
       <h1> Logo </h1>
-      <ul className="flex gap-8">
-        <li> Page 1</li>
-        <li> Page 1</li>
-        <li> Page 1</li>
-        <li> Page 1</li>
-      </ul>
+        {
+          userInfo ?
+          <NavbarUserInfo /> :
+          <ul className="flex gap-8">
+            <Link to="/login">
+              <li> Login </li>
+            </Link>
+            <Link to="/signup">
+              <li> Sign Up </li>
+            </Link>
+          </ul>
+        }
     </nav>
   )
 }
