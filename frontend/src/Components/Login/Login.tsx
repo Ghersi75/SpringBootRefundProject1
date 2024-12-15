1497c1ab12d650692bf77371c7ef727e932a9a5a
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-15 14:40:33 -0500

    Created basic login logic

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -2,13 +2,16 @@ import { useContext, useState } from "react"
 // import { useNavigate } from "react-router-dom";
 import { UserContext } from "../../Context/UserContext";
 import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import LeaveIfLoggedIn from "../LeaveIfLoggedIn";
 
 
 export default function Login() {
   const [showPass, setShowPass] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [cookies, setCookies] = useCookies(["user"]);
+  const [cookies, _] = useCookies(["user"]);
+  const navigate = useNavigate();
   // const navigate = useNavigate();
   console.log(cookies);
   const content = useContext(UserContext);
@@ -38,25 +41,40 @@ export default function Login() {
     // })
 
     await fetch("http://localhost:8080/user/login", {
-      method: "GET",
+      method: "POST",
+      body: JSON.stringify({
+        email: username,
+        password: password
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      },
       credentials: "include"
     })
       .then(res => res.json())
-      .then(data => { console.log(data); })
+      .then(data => { 
+        console.log(data);
+        if ((data as any).success == true) {
+          // navigate(0);
+          console.log("Done")
+        }
+      })
   }
 
   return (
-    <div className="grow flex justify-center items-center">
-      <form className="bg-zinc-800 p-8 rounded-l flex flex-col gap-4" onSubmit={(e) => {e.preventDefault()}}>
-        <label className="text-white"> Username: </label>
-        <input className="bg-zinc-700 rounded p-2 text-white"  onChange={handleUsernameChange} value={username} />
-        <label className="text-white"> Password: </label>
-        <div className="relative">
-          <input className="bg-zinc-700 rounded p-2 text-white" type={!showPass ? "password" : ""} onChange={handlePasswordChange} value={password}/>
-          <div className="absolute right-0 top-1/2 -translate-y-1/2 right-4 text-white hover:underline cursor-pointer" onClick={handleShowPassword}> show </div>
-        </div>
-        <button className="bg-zinc-700 p-2 rounded text-white" onClick={handleLogin}> Log in </button>
-      </form>
-    </div>
+    <LeaveIfLoggedIn>
+      <div className="grow flex justify-center items-center">
+        <form className="bg-zinc-800 p-8 rounded-l flex flex-col gap-4" onSubmit={(e) => {e.preventDefault()}}>
+          <label className="text-white"> Username: </label>
+          <input className="bg-zinc-700 rounded p-2 text-white"  onChange={handleUsernameChange} value={username} />
+          <label className="text-white"> Password: </label>
+          <div className="relative">
+            <input className="bg-zinc-700 rounded p-2 text-white" type={!showPass ? "password" : ""} onChange={handlePasswordChange} value={password}/>
+            <div className="absolute right-0 top-1/2 -translate-y-1/2 right-4 text-white hover:underline cursor-pointer" onClick={handleShowPassword}> show </div>
+          </div>
+          <button className="bg-zinc-700 p-2 rounded text-white" onClick={handleLogin}> Log in </button>
+        </form>
+      </div>
+    </LeaveIfLoggedIn>
   )
 }
