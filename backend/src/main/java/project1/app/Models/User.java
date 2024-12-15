522e04be0f9911d0dacf1c7ed98f67de4d275835
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-15 16:47:29 -0500

    Updated User entity to reference and return a list of all tickets posted by that user

--- backend/src/main/java/project1/app/Models/User.java
+++ backend/src/main/java/project1/app/Models/User.java
@@ -1,5 +1,10 @@
 package project1.app.Models;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -7,6 +12,7 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -44,6 +50,15 @@ public class User {
   @Enumerated(EnumType.STRING)
   @Column(name = "user_role")
   private Role userRole;
+
+  // mappedBy refers to the name of the variable in the ticket entity, which is user in this case (private User user)
+  // cascade makes sure that anything referencing this user will update all associated tickets
+  // orphanRemoval gets rid of any tickets that are no longer associated with this user
+  @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+  // https://stackoverflow.com/questions/31319358/jsonmanagedreference-vs-jsonbackreference for reference
+  // Prevents infinite recurion when User has Ticker and Ticker has User as a field
+  @JsonManagedReference
+  private List<Ticket> tickets;
 
   public User(UserSignUpDTO userInfo) {
     this.username = userInfo.getUsername();
