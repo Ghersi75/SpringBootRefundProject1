903e79786a7632f1fd2998539234fea08364cc29
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-15 15:01:30 -0500

    Added react hot toast for toast notification

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import Login from "./Components/Login/Login"
 import Navbar from "./Components/Navbar/Navbar"
 import Tickets from "./Pages/Tickets"
 import SignUpManager from "./Components/SignUp/SignUpManager"
+import { Toaster } from "react-hot-toast"
 
 function App() {
   return (
     <div className="min-h-svh min-w-svw bg-zinc-900 flex flex-col">
+      <Toaster />
       <Navbar />
       <Routes>
         <Route path="/" element= { <Tickets /> } />
