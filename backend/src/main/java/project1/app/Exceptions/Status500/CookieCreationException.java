9ccb2ab7126b008be62d9fff14e1539dcf477a5d
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-15 14:18:52 -0500

    Created exception for cookie creation error

---
+++ backend/src/main/java/project1/app/Exceptions/Status500/CookieCreationException.java
@@ -0,0 +1,7 @@
+package project1.app.Exceptions.Status500;
+
+public class CookieCreationException extends Status500Exception {
+  public CookieCreationException(String msg) {
+    super(msg);
+  }
+}
