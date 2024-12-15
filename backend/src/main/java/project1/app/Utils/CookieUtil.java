8376d43b715970831cefb82425eaf2d480fc33ca
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-15 13:54:44 -0500

    Created CookieUtil for cookie creation. Currently used for userInfo for frontend, but can be used for jwt and any other cookies needed

---
+++ backend/src/main/java/project1/app/Utils/CookieUtil.java
@@ -0,0 +1,33 @@
+package project1.app.Utils;
+
+import java.net.URLEncoder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.servlet.http.Cookie;
+
+public class CookieUtil {
+  public static Cookie CreateCookie(String key, Object value, String path, boolean httpOnly, boolean secure, int maxAge) {
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+      // Throws RuntimeException so Transactional would rollback automatically
+      String objectAsString = objectMapper.writeValueAsString(value);
+      // Throws checked Exception so Transaction rollbackFor would kick in and revert the tranasaction
+      String EncodedObjectAsString = URLEncoder.encode(objectAsString, "UTF-8");
+      Cookie cookie = new Cookie(key, EncodedObjectAsString);
+      // Make it available on the entire website instaed of the path it makes the api call from
+      cookie.setPath(path);
+      // Allow the cookie to be accessed through javascript
+      cookie.setHttpOnly(httpOnly);
+      // Only send the cookie through HTTPS
+      cookie.setSecure(secure);
+      // Setting the cookie age to -1 defaults to deleting once the session is over (close browser)
+      // Could set this to any number (of seconds) and the cookie will be deleted by the browser once it expires
+      cookie.setMaxAge(maxAge);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    return new Cookie("nice", "nice");
+  }
+}
