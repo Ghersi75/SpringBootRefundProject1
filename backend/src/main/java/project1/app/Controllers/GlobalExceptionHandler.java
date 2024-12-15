288955dd79ffdce45ec9a0fc8a7a7d4291f01fcd
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-14 21:32:28 -0500

    Added status 409 and 500 exception handlers to the GlobalExceptionHandler

--- backend/src/main/java/project1/app/Controllers/GlobalExceptionHandler.java
+++ backend/src/main/java/project1/app/Controllers/GlobalExceptionHandler.java
@@ -3,6 +3,10 @@ package project1.app.Controllers;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import project1.app.Exceptions.Status409.Status409Exception;
+import project1.app.Exceptions.Status500.Status500Exception;
+
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 import java.util.Map;
@@ -13,6 +17,24 @@ import org.springframework.validation.FieldError;
 // Not too familiar with this, but it's typically used for Exception Handler classes
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+  // Status 409 - Conflict
+  // Thrown if a unique user field is already taken
+  // Example: User SignUp username or email already taken
+  @ExceptionHandler(Status409Exception.class)
+  @ResponseStatus(HttpStatus.CONFLICT)
+  public Map<String, String> Status409Handler(Status409Exception e) {
+    return Map.of("error", e.getMessage());
+  }
+
+  // Status 500 - Internal Server Error
+  // Thrown if something outside of user control goes wrong
+  // Example: User creation goes wrong
+  @ExceptionHandler(Status500Exception.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public Map<String, String> Status500Handler(Status500Exception e) {
+    return Map.of("error", e.getMessage());
+  }
+
   // Status 400 - Bad Request
   // This will hit if any of the request body is invalid
   // Each field is checked by jakarta and appropriate messages will be returned
