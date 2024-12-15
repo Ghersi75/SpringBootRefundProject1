9ad915b22abafc021cb33eef59a85590407b8af1
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-15 14:41:12 -0500

    Updated username to be stored with spaces instead of +. When cookies are created all spaces are turned into +, which is not what we want displayed

--- frontend/src/Context/UserContext.tsx
+++ frontend/src/Context/UserContext.tsx
@@ -1,12 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { ChildrenPropType, UserContextType, UserInfoType } from "../Types/ContextTypes";
 import { useCookies } from "react-cookie";
 
 export const UserContext = createContext<UserContextType | null>(null);
 
+// Fancy name for a simple function
+// Currently spaces in username are being replaced with + to store them properly as cookies
+// This is just annoying for username display
+// Technically if a user includes a + in their name it would just be changed to a space as well
+// TODO: Change username constraints to alphanumerical only in the backend
+const decodeUsername = (username: string) => {
+  return username.replace("+", " ");
+}
+
 export const UserProvider = ({ children }: ChildrenPropType) => {
   const [userCookie, _] = useCookies(["userInfo"]);
-  const [userInfo, setUserInfo] = useState<UserInfoType | null>(userCookie.userInfo);
+  const decodedUsername = decodeUsername(userCookie.userInfo.username);
+  const [userInfo, setUserInfo] = useState<UserInfoType | null>({ ...userCookie.userInfo, username: decodedUsername });
+
+  // Update if cookie changes or is removed
+  useEffect(() => {
+    setUserInfo({ ...userCookie.userInfo, username: decodedUsername });
+  }, [userCookie.userInfo])
+
   return (
     <UserContext.Provider value={{ userInfo, setUserInfo }}>
       { children }
