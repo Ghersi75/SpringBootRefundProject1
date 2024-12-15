63098cf1d8a33425c2289c6f99f1a709cb220a2d
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-14 21:15:27 -0500

    Updated cookie name

--- frontend/src/Context/UserContext.tsx
+++ frontend/src/Context/UserContext.tsx
@@ -5,8 +5,8 @@ import { useCookies } from "react-cookie";
 export const UserContext = createContext<UserContextType | null>(null);
 
 export const UserProvider = ({ children }: ChildrenPropType) => {
-  const [userCookie, _] = useCookies(["user"]);
-  const [userInfo, setUserInfo] = useState<UserInfoType | null>(userCookie.user);
+  const [userCookie, _] = useCookies(["userInfo"]);
+  const [userInfo, setUserInfo] = useState<UserInfoType | null>(userCookie.userInfo);
   return (
     <UserContext.Provider value={{ userInfo, setUserInfo }}>
       { children }
