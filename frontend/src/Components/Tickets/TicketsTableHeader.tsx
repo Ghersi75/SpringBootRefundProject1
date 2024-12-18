17327e53f62f768f82c76baca02bdb929822d998
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-18 15:20:59 -0500

    Renamed TicketTopRow to TicketsTableHeader

--- frontend/src/Components/Tickets/TicketsTableHeader.tsx
+++ frontend/src/Components/Tickets/TicketsTableHeader.tsx
@@ -1,4 +1,4 @@
-export default function TicketsTopRow() {
+export default function TicketsTableHeader() {
   return (
     <thead className="sticky top-0 bg-zinc-700 text-white">
       <tr>
@@ -6,7 +6,7 @@ export default function TicketsTopRow() {
         <th className="p-4 w-[150px] text-center">Amount</th>
         <th className="p-4 text-left">Description</th>
         <th className="p-4 w-[150px] text-center">Ticket Status</th>
-        <th className="p-4 w-[150px] text-center">Time Added</th>
+        <th className="p-4 w-[200px] text-center">Time Added</th>
         <th className="p-4 w-[150px] text-center">Reimbursement Type</th>
       </tr>
     </thead>
