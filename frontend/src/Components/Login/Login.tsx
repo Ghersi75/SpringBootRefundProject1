e47c5f46d329224b994307ef016af00da77d7e7b
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-18 18:40:24 -0500

    Removed old route guard

--- frontend/src/Components/Login/Login.tsx
+++ frontend/src/Components/Login/Login.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react"
-// import { useNavigate } from "react-router-dom";
-import LeaveIfLoggedIn from "../LeaveIfLoggedIn";
 import { LoginFormPropsType } from "../../Types/UserFormTypes";
 
 
@@ -16,19 +14,17 @@ export default function Login({ email, setEmail, password, setPassword, handleLo
   }
 
   return (
-    <LeaveIfLoggedIn>
-      <div className="grow flex justify-center items-center">
-        <form className="bg-zinc-800 p-8 rounded-l flex flex-col gap-4" onSubmit={(e) => {e.preventDefault()}}>
-          <label className="text-white"> Email: </label>
-          <input className="bg-zinc-700 rounded p-2 text-white"  onChange={(e) => { handleChange(e, setEmail) }} value={email} />
-          <label className="text-white"> Password: </label>
-          <div className="relative">
-            <input className="bg-zinc-700 rounded p-2 text-white" type={!showPass ? "password" : ""} onChange={(e) => { handleChange(e, setPassword) }} value={password}/>
-            <div className="absolute right-0 top-1/2 -translate-y-1/2 right-4 text-white hover:underline cursor-pointer" onClick={handleShowPassword}> show </div>
-          </div>
-          <button className="bg-zinc-700 p-2 rounded text-white" onClick={handleLogin}> Log in </button>
-        </form>
-      </div>
-    </LeaveIfLoggedIn>
+    <div className="grow flex justify-center items-center">
+      <form className="bg-zinc-800 p-8 rounded-l flex flex-col gap-4" onSubmit={(e) => {e.preventDefault()}}>
+        <label className="text-white"> Email: </label>
+        <input className="bg-zinc-700 rounded p-2 text-white"  onChange={(e) => { handleChange(e, setEmail) }} value={email} />
+        <label className="text-white"> Password: </label>
+        <div className="relative">
+          <input className="bg-zinc-700 rounded p-2 text-white" type={!showPass ? "password" : ""} onChange={(e) => { handleChange(e, setPassword) }} value={password}/>
+          <div className="absolute right-0 top-1/2 -translate-y-1/2 right-4 text-white hover:underline cursor-pointer" onClick={handleShowPassword}> show </div>
+        </div>
+        <button className="bg-zinc-700 p-2 rounded text-white" onClick={handleLogin}> Log in </button>
+      </form>
+    </div>
   )
 }
