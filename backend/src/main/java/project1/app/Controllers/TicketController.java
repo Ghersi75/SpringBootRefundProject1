3549125232b643e49845c757d7e91b252d119ec3
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-18 14:04:23 -0500

    Added JWT checking and extracting user role and id from it. Also added temporary status 500 exceptions for now.

--- backend/src/main/java/project1/app/Controllers/TicketController.java
+++ backend/src/main/java/project1/app/Controllers/TicketController.java
@@ -1,8 +1,9 @@
 package project1.app.Controllers;
 
 import java.util.List;
-import java.util.Map;
 
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,13 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import project1.app.DTO.AuthUserInfoDTO;
 import project1.app.DTO.NewTicketDTO;
 import project1.app.Enums.TicketStatus;
+import project1.app.Enums.UserRole;
+import project1.app.Exceptions.Status500.Status500Exception;
 import project1.app.Models.Ticket;
 import project1.app.Service.TicketService;
+import project1.app.Utils.JWTUtil;
 
 @RestController
 @RequestMapping("/ticket")
+@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class TicketController {
   private TicketService ticketService;
 
@@ -25,14 +31,42 @@ public class TicketController {
   }
   
   @GetMapping("")
-  public List<Ticket> getAllTicketsHandler(@RequestParam(required = false) Long userId, @RequestParam(required = false) String ticketStatus ) {
-    // TicketStatus ticketStatusEnum;
-    return this.ticketService.getAllTicketsFiltered(userId, TicketStatus.fromString(ticketStatus));
+  public List<Ticket> getAllTicketsHandler(@RequestParam(required = false) Long userId, @RequestParam(required = false) String ticketStatus, @CookieValue(value = "jwt", defaultValue = "") String jwt) {
+    if (jwt.isEmpty()) {
+      // TODO: Not authorized, clear cookie and sign out
+      throw new Status500Exception("Missing JWT");
+    }
+    AuthUserInfoDTO userInfoDTO = JWTUtil.extractUserInfoFromJWT(jwt);
+    switch (userInfoDTO.getUserRole()) {
+      case EMPLOYEE:
+        if (userId != null && userId != userInfoDTO.getUserId()) {
+          // Employee can't request other employees' tickets
+          // TODO: Fix exception thrown
+          throw new Status500Exception("Not authorized");
+        }
+        return this.ticketService.getAllTicketsFiltered(userInfoDTO.getUserId(), TicketStatus.fromString(ticketStatus));
+      // Since we only have two options and switch needs a default, MANAGER can be default for this case
+      // TODO: Figure out a proper way of doing this
+      default:
+        // Managers only care about pending tickets
+        return this.ticketService.getAllTicketsFiltered(userId, TicketStatus.PENDING);
+    }
   }
 
   @PostMapping("")
-  public Ticket createTicket(@RequestBody NewTicketDTO ticketInfo) {
+  public Ticket createTicket(@RequestBody NewTicketDTO ticketInfo, @CookieValue(value = "jwt", defaultValue = "") String jwt) {
+    if (jwt.isEmpty()) {
+      // TODO: Not authorized, clear cookie and sign out
+      throw new Status500Exception("Missing JWT");
+    }
+    AuthUserInfoDTO userInfoDTO = JWTUtil.extractUserInfoFromJWT(jwt);
+    
+    if (userInfoDTO.getUserRole() == UserRole.MANAGER) {
+      // TODO: Create proper exception
+      throw new Status500Exception("Only Employees can create tickets");
+    }
+
     // TODO: Once JWT is implemented, get userId from it
-    return this.ticketService.createTicket(ticketInfo);
+    return this.ticketService.createTicket(ticketInfo, userInfoDTO.getUserId());
   }
 }
