c669c06d99eb60e34c0fa4d564ba49d867778bc7
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-18 15:20:07 -0500

    Updated amount to use built in toLocaleString for currency formatting

--- frontend/src/Components/Tickets/Ticket.tsx
+++ frontend/src/Components/Tickets/Ticket.tsx
@@ -11,7 +11,7 @@ export default function Ticket({
   return (
     <tr className=" odd:bg-zinc-400 even:bg-zinc-500 text-white">
       <td className="p-4 text-center">{id}</td>
-      <td className="p-4 w-[150px] text-center">{amount}</td>
+      <td className="p-4 w-[150px] text-center">{(amount / 100).toLocaleString("en-US", { style: "currency", currency: "USD" })}</td>
       <td className="p-4 text-left h-[100px] overflow-auto">{description}</td>
       <td className="p-4 w-[150px] text-center">{ticketStatus}</td>
       <td className="p-4 w-[150px] text-center">{timeAdded}</td>
