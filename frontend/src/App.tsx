104ce40e774f48d648df2130334fc1d0242f9cdf
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-19 13:41:22 -0500

    Added conditional rendering of ticket page based on user role

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,15 +3,19 @@ import Navbar from "./Components/Navbar/Navbar"
 import SignUpManager from "./Components/SignUp/SignUpManager"
 import { Toaster } from "react-hot-toast"
 import LoginManager from "./Components/Login/LoginManager"
-import TicketsManager from "./Components/Tickets/Employee/EmployeeTicketsManager"
+import { useUserInfo } from "./Hooks/useUserInfo"
+import ManagerTicketsManager from "./Components/Tickets/Manager/ManagerTicketsManager"
+import EmployeeTicketsManager from "./Components/Tickets/Employee/EmployeeTicketsManager"
 
 function App() {
+  const { userInfo } = useUserInfo();
+
   return (
     <div className="h-svh max-w-svw bg-zinc-900 flex flex-col">
       <Toaster />
       <Navbar />
       <Routes>
-        <Route path="/" element= { <TicketsManager /> } />
+        <Route path="/" element= { userInfo?.userRole == "MANAGER" ? <ManagerTicketsManager /> : <EmployeeTicketsManager /> } />
         <Route path="/login" element={ <LoginManager /> } />
         <Route path="/signup" element={ <SignUpManager /> } />
       </Routes>
