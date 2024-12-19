0580b106fad9145d8dbe266ac77e8a4e131c1d34
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-19 13:40:42 -0500

    Updated to show userId next to username

--- frontend/src/Components/Navbar/NavbarUserInfo.tsx
+++ frontend/src/Components/Navbar/NavbarUserInfo.tsx
@@ -7,14 +7,14 @@ export default function NavbarUserInfo() {
     return ;
   }
 
-  const { username, email, profilePicLink } = userInfo;
+  const { username, email, userId, profilePicLink } = userInfo;
   return(
-    <div className="flex gap-4">
+    <div className="flex items-center gap-4">
       <div className="flex flex-col justify-center">
-        <h1> {username} </h1>
+        <h1> {`${username} { id: ${userId} }`} </h1>
         <h1> {email} </h1>
       </div>
-      <img src={profilePicLink ? profilePicLink : "https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg"} className="w-[50px] rounded-full"/>
+      <img src={profilePicLink ? profilePicLink : "https://t4.ftcdn.net/jpg/05/49/98/39/360_F_549983970_bRCkYfk0P6PP5fKbMhZMIb07mCJ6esXL.jpg"} className="w-[50px] h-[50px] rounded-full"/>
     </div>
   )
 }
