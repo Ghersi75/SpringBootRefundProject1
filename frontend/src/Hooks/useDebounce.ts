cd699827c0ac535f287315a90db8459a58bc4ab9
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-19 14:25:44 -0500

    switched to axios, check for empty username and return, and set is available to null while checking username availability

--- frontend/src/Hooks/useDebounce.ts
+++ frontend/src/Hooks/useDebounce.ts
@@ -1,19 +1,23 @@
 import { useEffect, useState } from "react";
-import { IsUsernameAvailableType } from "../Types/APIReturnTypes";
+import axios from "axios";
 
 // https://stackoverflow.com/questions/77123890/debounce-in-reactjs
 export const useDebounce = (username: string, delay: number) => {
   const [isAvailable, setIsAvailable] = useState<boolean | null>(null);
   useEffect(() => {
+    if (username == "") {
+      return;
+    }
+    // When it's null its being checked, uselful for sign up component
+    setIsAvailable(null);
     const handler = setTimeout(async () => {
-      await fetch(`http://localhost:8080/user/check-username?username=${username}`)
-        .then((res) => res.json())
-        .then((data: IsUsernameAvailableType) => setIsAvailable(data.isAvailable));
-      }, delay);
-      
-      return (() => {
-        clearTimeout(handler);
-      })
+      await axios.get(`http://localhost:8080/user/check-username?username=${username}`)
+        .then((res) => setIsAvailable(res.data.isAvailable));
+    }, delay);
+
+    return (() => {
+      clearTimeout(handler);
+    })
   }, [username]);
 
   return isAvailable;
