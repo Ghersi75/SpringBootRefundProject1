a3fc00093dea55338c8e13e9ddffa08987f16c18
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-19 17:19:00 -0500

    updated setup and added tests for SignUpUser that should cover all cases

--- backend/src/test/java/project1/app/ServiceTests/UserServiceTests.java
+++ backend/src/test/java/project1/app/ServiceTests/UserServiceTests.java
@@ -17,6 +17,14 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
 import project1.app.Service.UserService;
+import project1.app.DTO.AuthUserInfoDTO;
+import project1.app.DTO.DisplayUserInfoDTO;
+import project1.app.DTO.UserInfoDTO;
+import project1.app.DTO.UserSignUpDTO;
+import project1.app.Enums.UserRole;
+import project1.app.Exceptions.Status409.EmailAlreadyTakenException;
+import project1.app.Exceptions.Status409.UsernameAlreadyTakenException;
+import project1.app.Exceptions.Status500.UserCreationError;
 import project1.app.Models.User;
 import project1.app.Repository.UserRepository;
 
@@ -37,9 +45,10 @@ public class UserServiceTests {
     User user1 = new User();
     user1.setId(1L);
     user1.setUsername("user1");
+    user1.setEmail("user1@email.com");
     User user2 = new User();
     user2.setId(2L);
-    user1.setUsername("ghersi");
+    user2.setUsername("ghersi");
     user2.setEmail("testemail@test.com");
 
     // getAllUsers
@@ -47,19 +56,31 @@ public class UserServiceTests {
 
     // isUsernameAvailable
     // https://davidvlijmincx.com/posts/mockito_thenreturn_and_thenanswer/
-    when(userRepository.findByUsernameIgnoreCase("user1")).thenReturn(user1);
-    when(userRepository.findByUsernameIgnoreCase("ghersi")).thenReturn(user2);
-
     when(userRepository.findByUsernameIgnoreCase(Mockito.anyString())).thenAnswer(invocation -> {
-      String username = invocation.getArgument(0, String.class).toLowerCase();
+      String username = invocation.getArgument(0);
 
-      if (username.equalsIgnoreCase(user1.getUsername()) || username.equalsIgnoreCase(user2.getUsername())) {
-        // This will call methods defined above with actual values
-        return invocation.callRealMethod();
+      if (username.equalsIgnoreCase(user1.getUsername())) {
+        return user1;
+      } else if (username.equalsIgnoreCase(user2.getUsername())) {
+        return user2;
       }
 
       return null;
     });
+    // SignUpUser
+    when(userRepository.findByEmailIgnoreCase(Mockito.anyString())).thenAnswer(invocation -> {
+      String email = invocation.getArgument(0);
+
+      if (email.equalsIgnoreCase("user1@gmail.com")) {
+        return user1;
+      } else if (email.equalsIgnoreCase("testemail@test.com")) {
+        return user2;
+      }
+
+      return null;
+    });
+
+    System.out.println("Email: " + this.userRepository.findByEmailIgnoreCase("testemail@test.com"));
   }
 
   @Test
@@ -79,5 +100,42 @@ public class UserServiceTests {
     assertEquals(true, availableUsername);
   }
 
+  @Test
+  public void testSignUpUser() {
+    UserSignUpDTO userInfo = new UserSignUpDTO();
 
+    // Username already taken
+    userInfo.setUsername("user1");
+    assertThrows(UsernameAlreadyTakenException.class, () -> this.userService.SignUpUser(userInfo));
+    userInfo.setUsername("unique username");
+
+    // Email already taken
+    userInfo.setEmail("user1@gmail.com");
+    assertThrows(EmailAlreadyTakenException.class, () -> this.userService.SignUpUser(userInfo));
+    userInfo.setEmail("uniquemail@gmail.com");
+
+    // Simulate user creation failing
+    when(this.userRepository.findByUsernameIgnoreCase(Mockito.anyString())).thenReturn(null);
+    assertThrows(UserCreationError.class, () -> this.userService.SignUpUser(userInfo));
+
+    // Simulate new user being created
+    // .thenReturn chained simulates multiple calls
+    // Since service first checks that username is not taken, it should return null
+    // first and then the created user second
+    when(this.userRepository.findByUsernameIgnoreCase(userInfo.getUsername()))
+        .thenReturn(null)
+        .thenAnswer(invocation -> {
+          User newUser = new User(userInfo);
+          newUser.setId(3L);
+
+          return newUser;
+
+        });
+
+    // new User defaults to EMPLOYEE if not set
+    DisplayUserInfoDTO displayUserInfo = new DisplayUserInfoDTO(userInfo.getUsername(), userInfo.getEmail(), 3L,
+        UserRole.EMPLOYEE);
+    AuthUserInfoDTO authUserInfo = new AuthUserInfoDTO(3L, UserRole.EMPLOYEE);
+    assertEquals(new UserInfoDTO(displayUserInfo, authUserInfo), this.userService.SignUpUser(userInfo));
+  }
 }
