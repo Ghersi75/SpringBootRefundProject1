511698832365fe5a1e0d0dbbf6100ff807108c8d
Ghersi75 <34931324+Ghersi75@users.noreply.github.com>
2024-12-19 13:41:04 -0500

    Added logic to approve/deny tickets on the manager ticket page

--- frontend/src/Components/Tickets/Manager/Ticket.tsx
+++ frontend/src/Components/Tickets/Manager/Ticket.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { TicketProps } from "../../../Types/TicketProps";
+import { useNavigate } from "react-router-dom";
 
 export default function Ticket({
   id,
@@ -7,14 +9,29 @@ export default function Ticket({
   timeAdded,
   reimbursementType,
 }: TicketProps) {
+  const navigate = useNavigate();
+
+  const handleClick = (ticketStatus: string) => {
+    axios.patch("http://localhost:8080/ticket", {
+      "ticketId": id,
+      "ticketStatus": ticketStatus
+    }, {
+      withCredentials: true
+    }).then(res => {
+      console.log(res)
+      // Refresh page
+      navigate(0);
+    })
+  }
+
   return (
     <tr className=" odd:bg-zinc-400 even:bg-zinc-500 text-white">
       <td className="p-4 text-center">{id}</td>
       <td className="p-4 w-[150px] text-center">{(amount / 100).toLocaleString("en-US", { style: "currency", currency: "USD" })}</td>
       <td className="p-4 text-left h-[100px] overflow-auto">{description}</td>
       <td className="p-4 text-center grid gap-2">
-        <button className="p-2 bg-green-700 px-4 rounded-xl"> Approve </button>
-        <button className="p-2 bg-red-700 px-4 rounded-xl"> Deny </button>
+        <button className="p-2 bg-green-700 px-4 rounded-xl" onClick={() => {handleClick("APPROVED")}}> Approve </button>
+        <button className="p-2 bg-red-700 px-4 rounded-xl" onClick={() => {handleClick("DENIED")}}> Deny </button>
       </td>
       <td className="p-4 w-[150px] text-center">{timeAdded}</td>
       <td className="p-4 w-[150px] text-center">{reimbursementType}</td>
